ca895f13a4481a9c34f36e64af2db13f30206b4e
Oleksii <semolex@live.com>
2018-04-06 13:23:01 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='zfdb',
+    version='0.1',
+    packages=[],
+    url='https://github.com/semolex/zfdb',
+    license='MIT',
+    author='semolex (Oleksii Semeshchuk)',
+    author_email='semolex@live.com',
+    description='Simple interface to the ZIP files to use them as DB'
+)
